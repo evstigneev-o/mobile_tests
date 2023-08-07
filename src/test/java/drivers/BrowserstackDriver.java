50b3f4c3659b033898d21ca0b3efefdcc84b60f7
Oleg Evstigneev <o.evstigneev@citydrive.ru>
2023-08-07 20:31:58 +0300

    update driver

--- src/test/java/drivers/BrowserstackDriver.java
+++ src/test/java/drivers/BrowserstackDriver.java
@@ -21,11 +21,13 @@ public class BrowserstackDriver implements WebDriverProvider {
         caps.setCapability("browserstack.key", "GvsVzaDY76sHyVY2fLnn");
 
         // Set URL of the application under test
-        caps.setCapability("app", "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827.app");
+        caps.setCapability("app", "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c");
 
         // Specify device and os_version for testing
         caps.setCapability("device", "Google Pixel 3");
         caps.setCapability("os_version", "9.0");
+//        caps.setCapability("appium:deviceName", "iPhone 14 Pro");
+//        caps.setCapability("appium:platformVersion", "16");
 
         // Set other BrowserStack capabilities
         caps.setCapability("project", "First Java Project");
